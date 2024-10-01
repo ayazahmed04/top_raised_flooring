b9c64b9b96a59372d542fc5322d1b3a2a57633c8
yazi <forgithub44@gmail.com>
2024-10-01 11:13:48 +0400

    Link to Kevin Email to emailjs

--- src/Routes/Emailjs.jsx
+++ src/Routes/Emailjs.jsx
@@ -1,91 +1,113 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com'
 
-// Import Header and footer for our application  
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+// Import Header and footer for our application
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
+   const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      message: '',
+   })
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    emailjs.send('service_ft3hmwk', 'template_ux9tiov', formData, 'xt7zfvlSNC7XJE-m1')
-      .then((response) => {
-        alert('Message sent successfully!');
+   const handleChange = (e) => {
+      setFormData({
+         ...formData,
+         [e.target.name]: e.target.value,
       })
-      .catch((error) => {
-        console.error('Error sending message:', error);
-        alert('Error sending message.');
-      });
-  };
+   }
 
-  return (
-    <div>
-      <Header />
-    <div className="max-w-lg mx-auto p-6 mt-28 bg-white shadow-lg rounded-lg">
-      
-      <h2 className="text-2xl font-bold mb-4  text-gray-800">Contact Us</h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div>
-          <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-            rows="4"
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Send
-        </button>
-      </form>
-    </div>
-    <Footer />
-    </div>
-  );
-};
+   const handleSubmit = (e) => {
+      e.preventDefault()
 
-export default ContactForm;
+      emailjs
+         .send(
+            'service_wf79zz4',
+            'template_3nl9tic',
+            formData,
+            'WrrKqAi1BZnXnMEmE'
+         )
+         .then((response) => {
+            alert('Message sent successfully!')
+         })
+         .catch((error) => {
+            console.error('Error sending message:', error)
+            alert('Error sending message.')
+         })
+   }
+
+   return (
+      <div>
+         <Header />
+         <div className="max-w-lg mx-auto p-6 mt-28 bg-white shadow-lg rounded-lg">
+            <h2 className="text-2xl font-bold mb-4  text-gray-800">
+               Contact Us
+            </h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+               <div>
+                  <label
+                     htmlFor="name"
+                     className="block text-sm font-medium text-gray-700"
+                  >
+                     Name
+                  </label>
+                  <input
+                     type="text"
+                     name="name"
+                     id="name"
+                     value={formData.name}
+                     onChange={handleChange}
+                     required
+                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+               </div>
+               <div>
+                  <label
+                     htmlFor="email"
+                     className="block text-sm font-medium text-gray-700"
+                  >
+                     Email
+                  </label>
+                  <input
+                     type="email"
+                     name="email"
+                     id="email"
+                     value={formData.email}
+                     onChange={handleChange}
+                     required
+                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+               </div>
+               <div>
+                  <label
+                     htmlFor="message"
+                     className="block text-sm font-medium text-gray-700"
+                  >
+                     Message
+                  </label>
+                  <textarea
+                     name="message"
+                     id="message"
+                     value={formData.message}
+                     onChange={handleChange}
+                     required
+                     rows="4"
+                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+               </div>
+               <button
+                  type="submit"
+                  className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+               >
+                  Send
+               </button>
+            </form>
+         </div>
+         <Footer />
+      </div>
+   )
+}
+
+export default ContactForm
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,88 +1,289 @@
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+   faTwitter,
+   faFacebook,
+   faInstagram,
+   faLinkedin,
+   faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
 
-import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebook, faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+const Footer = () => {
+   const form = useRef()
 
-const Footer  = () => {
-    const form = useRef();
+   const sendEmail = (e) => {
+      e.preventDefault()
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+      emailjs
+         .sendForm(
+            'service_wf79zz4',
+            'template_3nl9tic',
+            form.current,
+            'WrrKqAi1BZnXnMEmE'
+         )
+         .then(
+            (result) => {
+               console.log(result.text)
+               alert('Message sent successfully!')
+            },
+            (error) => {
+               console.log(error.text)
+               alert('An error occurred, please try again.')
+            }
+         )
+   }
 
-        emailjs.sendForm('service_ft3hmwk', 'template_ux9tiov', form.current, 'xt7zfvlSNC7XJE-m1')
-            .then((result) => {
-                console.log(result.text);
-                alert('Message sent successfully!');
-            }, (error) => {
-                console.log(error.text);
-                alert('An error occurred, please try again.');
-            });
-    };
+   return (
+      <div className="min-h-screen bg-gray-100">
+         <div className="container mt-24 mx-auto px-4">
+            <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
+               Contact Us
+            </h2>
 
-    return (
-        <div className="min-h-screen bg-gray-100 ">
-            <div className="container mt-24 mx-auto px-4">
-                <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">Contact Us</h2>
-                
-                {/* Contact Details */}
-                <div className="mb-8">
-                    <h3 className="text-2xl font-semibold text-gray-700">Get in Touch</h3>
-                    <p className="text-gray-600 mt-4">Feel free to reach out to us via the following channels:</p>
-                    <div className="mt-4">
-                        <p className="text-gray-800">Phone:  +971 56 5969 600</p>
-                        <p className="text-gray-800">Email: adraisedflooring@gmail.com</p>
-                        <p className="text-gray-800">Address: 113 - Jabal Ali Industrial Area 1, Dubai, United Arab Emirates</p>
-                    </div>
-                </div>
-
-                {/* Social Media Links */}
-                <div className="mb-8">
-                    <h3 className="text-2xl font-semibold text-gray-700">Follow Us</h3>
-                    <div className="flex space-x-4 mt-4">
-                        {/* <a href="#https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500">
-                            <FontAwesomeIcon icon={faTwitter} size="3x" />
-                        </a> */}
-                        <a href="https://www.facebook.com/profile.php?id=61565542242315" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-700">
-                            <FontAwesomeIcon icon={faFacebook} size="3x" />
-                        </a>
-                        {/* <a href="#https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-pink-500">
-                            <FontAwesomeIcon icon={faInstagram} size="3x" />
-                        </a>
-                        <a href="#https://linkedin.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-800">
-                            <FontAwesomeIcon icon={faLinkedin} size="3x" />
-                        </a> */}
-
-                        <a href="whatsapp://send?phone=+971 56 596 9600" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-800">
-                            <FontAwesomeIcon icon={faWhatsapp} target='_blank' size="3x" />
-                        </a>
-
-                    </div>
-                </div>
-
-                {/* Contact Form */}
-                <div className="bg-white p-8 rounded-lg shadow-lg">
-                    <h3 className="text-2xl font-semibold text-gray-700 mb-4">Send Us a Message</h3>
-                    <form ref={form} onSubmit={sendEmail}>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Name</label>
-                            <input type="text" name="user_name" className="w-full p-2 border border-gray-300 rounded mt-1" placeholder="Your Name" required />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Email</label>
-                            <input type="email" name="user_email" className="w-full p-2 border border-gray-300 rounded mt-1" placeholder="Your Email" required />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Message</label>
-                            <textarea name="message" className="w-full p-2 border border-gray-300 rounded mt-1" rows="4" placeholder="Your Message" required></textarea>
-                        </div>
-                        <button type="submit" className="bg-yellow-600 text-white px-4 py-2 rounded hover:bg-yellow-700">Send Message</button>
-                    </form>
-                </div>
+            {/* Contact Details */}
+            <div className="mb-8">
+               <h3 className="text-2xl font-semibold text-gray-700">
+                  Get in Touch
+               </h3>
+               <p className="text-gray-600 mt-4">
+                  Feel free to reach out to us via the following channels:
+               </p>
+               <div className="mt-4">
+                  <p className="text-gray-800">Phone: +971 56 5969 600</p>
+                  <p className="text-gray-800">
+                     Email: adraisedflooring@gmail.com
+                  </p>
+                  <p className="text-gray-800">
+                     Address: 113 - Jabal Ali Industrial Area 1, Dubai, United
+                     Arab Emirates
+                  </p>
+               </div>
             </div>
-            {/* <Footer /> */}
-        </div>
-    );
+
+            {/* Social Media */}
+            <div className="mb-8">
+               <h3 className="text-2xl font-semibold text-gray-700">
+                  Follow Us
+               </h3>
+               <div className="flex space-x-4 mt-4">
+                  <a
+                     href="https://www.facebook.com/profile.php?id=61565542242315"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="text-gray-500 hover:text-blue-700"
+                  >
+                     <FontAwesomeIcon icon={faFacebook} size="3x" />
+                  </a>
+                  <a
+                     href="whatsapp://send?phone=+971 56 596 9600"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="text-gray-500 hover:text-blue-800"
+                  >
+                     <FontAwesomeIcon icon={faWhatsapp} size="3x" />
+                  </a>
+               </div>
+            </div>
+
+            {/* Contact Form */}
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+               <h3 className="text-2xl font-semibold text-gray-700 mb-4">
+                  Send Us a Message
+               </h3>
+               <form ref={form} onSubmit={sendEmail}>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Name</label>
+                     <input
+                        type="text"
+                        name="user_name"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        placeholder="Your Name"
+                        required
+                     />
+                  </div>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Email</label>
+                     <input
+                        type="email"
+                        name="user_email"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        placeholder="Your Email"
+                        required
+                     />
+                  </div>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Phone Number</label>
+                     <input
+                        type="tel"
+                        name="user_phone"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        placeholder="Your Phone Number"
+                        required
+                     />
+                  </div>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Message</label>
+                     <textarea
+                        name="message"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        rows="4"
+                        placeholder="Your Message"
+                        required
+                     ></textarea>
+                  </div>
+                  <button
+                     type="submit"
+                     className="bg-yellow-600 text-white px-4 py-2 rounded hover:bg-yellow-700"
+                  >
+                     Send Message
+                  </button>
+               </form>
+            </div>
+         </div>
+      </div>
+   )
 }
 
-export default Footer;
+export default Footer
+
+/*
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+   faTwitter,
+   faFacebook,
+   faInstagram,
+   faLinkedin,
+   faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Footer = () => {
+   const form = useRef()
+
+   const sendEmail = (e) => {
+      e.preventDefault()
+
+      emailjs
+         .sendForm(
+            'service_wf79zz4',
+            'template_3nl9tic',
+            form.current,
+            'WrrKqAi1BZnXnMEmE'
+         )
+         .then(
+            (result) => {
+               console.log(result.text)
+               alert('Message sent successfully!')
+            },
+            (error) => {
+               console.log(error.text)
+               alert('An error occurred, please try again.')
+            }
+         )
+   }
+
+   return (
+      <div className="min-h-screen bg-gray-100 ">
+         <div className="container mt-24 mx-auto px-4">
+            <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
+               Contact Us
+            </h2>
+
+            <div className="mb-8">
+               <h3 className="text-2xl font-semibold text-gray-700">
+                  Get in Touch
+               </h3>
+               <p className="text-gray-600 mt-4">
+                  Feel free to reach out to us via the following channels:
+               </p>
+               <div className="mt-4">
+                  <p className="text-gray-800">Phone: +971 56 5969 600</p>
+                  <p className="text-gray-800">
+                     Email: adraisedflooring@gmail.com
+                  </p>
+                  <p className="text-gray-800">
+                     Address: 113 - Jabal Ali Industrial Area 1, Dubai, United
+                     Arab Emirates
+                  </p>
+               </div>
+            </div>
+
+            <div className="mb-8">
+               <h3 className="text-2xl font-semibold text-gray-700">
+                  Follow Us
+               </h3>
+               <div className="flex space-x-4 mt-4">
+                 <a
+                     href="https://www.facebook.com/profile.php?id=61565542242315"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="text-gray-500 hover:text-blue-700"
+                  >
+                     <FontAwesomeIcon icon={faFacebook} size="3x" />
+                     href="whatsapp://send?phone=+971 56 596 9600"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="text-gray-500 hover:text-blue-800"
+                  >
+                     <FontAwesomeIcon
+                        icon={faWhatsapp}
+                        target="_blank"
+                        size="3x"
+                     />
+                  </a>
+               </div>
+            </div>
+
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+               <h3 className="text-2xl font-semibold text-gray-700 mb-4">
+                  Send Us a Message
+               </h3>
+               <form ref={form} onSubmit={sendEmail}>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Name</label>
+                     <input
+                        type="text"
+                        name="user_name"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        placeholder="Your Name"
+                        required
+                     />
+                  </div>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Email</label>
+                     <input
+                        type="email"
+                        name="user_email"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        placeholder="Your Email"
+                        required
+                     />
+                  </div>
+                  <div className="mb-4">
+                     <label className="block text-gray-700">Message</label>
+                     <textarea
+                        name="message"
+                        className="w-full p-2 border border-gray-300 rounded mt-1"
+                        rows="4"
+                        placeholder="Your Message"
+                        required
+                     ></textarea>
+                  </div>
+                  <button
+                     type="submit"
+                     className="bg-yellow-600 text-white px-4 py-2 rounded hover:bg-yellow-700"
+                  >
+                     Send Message
+                  </button>
+               </form>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Footer
+
+*/
